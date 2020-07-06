584220db6f5115c3bcf31c9c465286c251575fae
Serban Gabriel <serban.gabriel.claudiu.13@gmail.com>
2020-07-06 02:45:49 -0700

    first commit

---
+++ src/components/counter.jsx
@@ -0,0 +1,36 @@
+import React, { Component, Fragment } from "react";
+
+class Counter extends Component {
+  render() {
+    return (
+      <Fragment>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.props.onIncrement(this.props.counter)}
+          className="btn btn-secondary bts-sm"
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
+      </Fragment>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { value: count } = this.props.counter;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
---
+++ src/components/counters.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import Counter from "./counter";
+
+import "../index.css";
+
+class Counters extends Component {
+  render() {
+    const { onReset, counters, onDelete, onIncrement } = this.props;
+    return (
+      <div>
+        <button onClick={onReset} className="btn btn-primary btn-sm m-2">
+          Reset
+        </button>
+        {counters.map((counter) => (
+          <li>
+            <Counter
+              key={counter.id}
+              counter={counter}
+              onDelete={onDelete}
+              onIncrement={onIncrement}
+            />
+          </li>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Counters;
